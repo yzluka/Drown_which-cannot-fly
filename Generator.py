2ac740fbd696a6191bea5e62dd6119efa869df2d
yzluka <zhany217@wfu.edu>
2020-06-26 15:35:44 -0400

    "initial commitment"

--- Generator.py
+++ Generator.py
@@ -14,28 +14,25 @@ def InfoMap(k=200, n=20, r=10):
 
 
 if __name__ == '__main__':
-    myDpi = 200
+    k, n, max_r = 200, 20, 10
+    myDpi = 400
     mpl.rcParams['figure.dpi'] = myDpi
     RawMap = InfoMap()
     print(RawMap)
-    fig = plt.figure(figsize=(6, 6), dpi=200, facecolor='w')
+    fig = plt.figure(figsize=(6, 6), dpi=myDpi, facecolor='w')
     ax = fig.add_axes([0, 0, 1, 1])
     plt.autoscale(False, tight=True)
-    plt.xlim(0, 200)
-    plt.ylim(0, 200)
-    plt.hlines(y=200, xmin=0, xmax=200, color="black")
-    plt.hlines(y=0, xmin=0, xmax=200, color="black")
-    plt.vlines(x=0, ymin=0, ymax=200, color="black")
-    plt.vlines(x=200, ymin=0, ymax=200, color="black")
+    plt.xlim(0, k)
+    plt.ylim(0, k)
+
     for dot in RawMap:
         ax.add_artist(plt.Circle((dot[0], dot[1]), dot[2], color='r', alpha=0.5))
-    ax.add_artist(plt.Circle((0, 0), 6, color='r', alpha=0.5))
+    ax.add_artist(plt.Circle((0, 0), 6, color='b', alpha=0.5))
+    ax.add_artist(plt.Circle((k, k), 6, color='b', alpha=0.5))  # verifying graph is correctly extracted
     ax.axis('square', adjustabble='box-force')
-    region = ax.transData.transform([(0, 0), (200, 200)])
-    print(region)
+    region = ax.transData.transform([(0, 0), (k, k)])
     ax.plot()
     BBOX = trs.Bbox(region / myDpi)
-
     plt.show()
     fig.savefig("testing1", dpi=myDpi, facecolor='w', bbox_inches=BBOX,
                 pad_inches=0)
