3dfeb5c3a452c0b6a8e11ace9ebc3483fe0d4b39
yzluka <zhany217@wfu.edu>
2020-06-27 00:13:01 -0400

    "setup mechanism for adding obstacle"

--- Generator.py
+++ Generator.py
@@ -5,7 +5,7 @@ from matplotlib import transforms as trs
 import matplotlib as mpl
 
 
-def InfoMap(k=200, n=20, r=10):
+def feature_gen(k=200, n=20, r=10):
     loc = []
     for _ in range(n):
         loc.append([np.random.randint(0, k), np.random.randint(0, k),
@@ -14,25 +14,31 @@ def InfoMap(k=200, n=20, r=10):
 
 
 if __name__ == '__main__':
-    k, n, max_r = 200, 20, 10
+    k1, n1, max_r1 = 200, 20, 10
     myDpi = 400
     mpl.rcParams['figure.dpi'] = myDpi
-    RawMap = InfoMap()
+    RawMap = feature_gen(k1, n1, max_r1)
     print(RawMap)
     fig = plt.figure(figsize=(6, 6), dpi=myDpi, facecolor='w')
     ax = fig.add_axes([0, 0, 1, 1])
     plt.autoscale(False, tight=True)
-    plt.xlim(0, k)
-    plt.ylim(0, k)
+    plt.xlim(0, k1)
+    plt.ylim(0, k1)
 
     for dot in RawMap:
         ax.add_artist(plt.Circle((dot[0], dot[1]), dot[2], color='r', alpha=0.5))
-    ax.add_artist(plt.Circle((0, 0), 6, color='b', alpha=0.5))
-    ax.add_artist(plt.Circle((k, k), 6, color='b', alpha=0.5))  # verifying graph is correctly extracted
+    # verifying graph is correctly extracted
     ax.axis('square', adjustabble='box-force')
-    region = ax.transData.transform([(0, 0), (k, k)])
+    region = ax.transData.transform([(0, 0), (k1, k1)])
     ax.plot()
     BBOX = trs.Bbox(region / myDpi)
+
+    fig.savefig("GC-testing1", dpi=myDpi, facecolor='w', bbox_inches=BBOX,
+                pad_inches=0)
+    Obstacle1 = feature_gen(k1, 8, 15)
+    for dot in Obstacle1:
+        ax.add_artist(plt.Circle((dot[0], dot[1]), dot[2], color='b', alpha=0.3))
+    ax.plot()
     plt.show()
     fig.savefig("testing1", dpi=myDpi, facecolor='w', bbox_inches=BBOX,
                 pad_inches=0)
--- testing.py
+++ testing.py
@@ -1,28 +1,28 @@
 import Simulator as SIM
 import numpy as np
 
-sim = SIM.ICRSsimulator('D:\\Drown\\data\\18JUL15152008-S3DS_R07C1-011089227010_01_P001_scaled (1).TIF')
+sim = SIM.ICRSsimulator('D:\\Drown_which-cannot-fly\\testing1.png')
 
 if not sim.loadImage():
     print("Error: could not load image")
     exit(0)
 
-lower = np.array([100, 39, 100])
-upper = np.array([200, 180, 150])
+# lower = np.array([, 39, 100])
+# upper = np.array([200, 180, 150])
 
-interestValue = 1  # Mark these areas as being of highest interest
-sim.classify('Mining', lower, upper, interestValue)
+# interestValue = 1  # Mark these areas as being of highest interest
+# sim.classify('Mining', lower, upper, interestValue)
 
-lower = np.array([0, 20, 0])
-upper = np.array([90, 157, 138])
-
-interestValue = 0  # Mark these areas as being of no interest
-sim.classify('Forest', lower, upper, interestValue)
-
-lower = np.array([70, 39, 142])
+lower = np.array([255, 255, 255])
 upper = np.array([255, 255, 255])
+
 interestValue = 0  # Mark these areas as being of no interest
-sim.classify('Water', lower, upper, interestValue)
+sim.classify('Background', lower, upper, interestValue)
+
+lower = np.array([0, 0, 255])
+upper = np.array([200, 200, 255])
+interestValue = 0  # Mark these areas as being of no interest
+sim.classify('target', lower, upper, interestValue)
 
 rows = 100
 cols = 100
