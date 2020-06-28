f190449d4e9ce77465a81280d920410709aeae08
yzluka <zhany217@wfu.edu>
2020-06-28 14:05:12 -0400

    "formalize code structure"

--- Generator.py
+++ Generator.py
@@ -14,30 +14,40 @@ def feature_gen(k=200, n=20, r=10):
 
 
 if __name__ == '__main__':
-    k1, n1, max_r1 = 200, 20, 10
+    k1, n1, max_r1 = 200, 20, 20
     myDpi = 400
     mpl.rcParams['figure.dpi'] = myDpi
-    RawMap = feature_gen(k1, n1, max_r1)
-    print(RawMap)
+
     fig = plt.figure(figsize=(6, 6), dpi=myDpi, facecolor='w')
     ax = fig.add_axes([0, 0, 1, 1])
     plt.autoscale(False, tight=True)
     plt.xlim(0, k1)
     plt.ylim(0, k1)
 
-    for dot in RawMap:
-        ax.add_artist(plt.Circle((dot[0], dot[1]), dot[2], color='r', alpha=0.5))
+
+    def load_feature(feature, shape='circle', color='r', alpha=0.5):
+        if shape == 'circle':
+            for dot in feature:
+                ax.add_artist(plt.Circle((dot[0], dot[1]), dot[2], ec='none', color=color, alpha=alpha))
+
+
+    RawMap = feature_gen(k1, n1, max_r1)
+    load_feature(RawMap)
+
     # verifying graph is correctly extracted
     ax.axis('square', adjustabble='box-force')
-    region = ax.transData.transform([(0, 0), (k1, k1)])
     ax.plot()
-    BBOX = trs.Bbox(region / myDpi)
 
+    region = ax.transData.transform([(0, 0), (k1, k1)])
+    BBOX = trs.Bbox(region / myDpi)
     fig.savefig("GC-testing1", dpi=myDpi, facecolor='w', bbox_inches=BBOX,
                 pad_inches=0)
-    Obstacle1 = feature_gen(k1, 8, 15)
-    for dot in Obstacle1:
-        ax.add_artist(plt.Circle((dot[0], dot[1]), dot[2], color='b', alpha=0.3))
+
+    Obstacle1 = feature_gen(k1, 15, 15)
+    fakeTarget = feature_gen(k1, 10, max_r1)
+
+    load_feature(Obstacle1, color='b', alpha=0.3)
+    load_feature(fakeTarget, color='#FF007F', alpha=0.5)
     ax.plot()
     plt.show()
     fig.savefig("testing1", dpi=myDpi, facecolor='w', bbox_inches=BBOX,
