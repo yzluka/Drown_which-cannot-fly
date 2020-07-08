c1f9d3d9ce571cf83e23225c42e51a3aec2f57d7
yzluka <zhany217@wfu.edu>
2020-07-08 14:29:47 -0400

    "Updated file transfer mechanism"

--- Reconstruct.py
+++ Reconstruct.py
@@ -1,4 +1,4 @@
-import Interpreter as Itp
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -9,4 +9,27 @@ def from_file(filename='InfoMap.npy'):
 
 if __name__ == '__main__':
     Map = from_file()
-    print(len(Map), len(Map[0]))
+    edgeSize = len(Map)
+
+    k = 200  # k is the number of
+    unitLength = k / edgeSize
+    myDpi = 400
+    mpl.rcParams['figure.dpi'] = myDpi
+
+    # 5.5 = 2000 pixel at dpi = 200. May scale up as demanded
+    fig = plt.figure(figsize=(5.5, 5.5), dpi=myDpi, facecolor='w')
+    ax = fig.add_axes([0, 0, 1, 1])
+    plt.autoscale(False, tight=True)
+    plt.xlim(0, k)
+    plt.ylim(0, k)
+
+    counterY = edgeSize
+
+    for i in Map:
+        counterY -= 1
+        counterX = 0
+        for j in i:
+            ax.add_artist(plt.Rectangle((counterX, counterY * unitLength), unitLength, unitLength, alpha=j / 400))
+            counterX += unitLength
+    ax.plot()
+    plt.show()
