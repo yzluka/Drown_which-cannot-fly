84efc888b05fcf48ea648f1c602503bb3b57cc6f
yzluka <zhany217@wfu.edu>
2020-10-24 21:35:41 -0400

    work in batch

--- sequence.py
+++ sequence.py
@@ -1,6 +1,6 @@
 import os
 
 os.system('py Generator.py')
-os.system('py blurring.py')
-os.system('py Interpreter.py')
-os.system('py Normalizer.py')
+# os.system('py blurring.py')
+# os.system('py Interpreter.py')
+# os.system('py Normalizer.py')
